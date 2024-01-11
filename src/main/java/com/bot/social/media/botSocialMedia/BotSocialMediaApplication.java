46562b98ead1a42a6b6b452592fdbb86ba23f6d9
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2024-01-11 08:36:13 -0500

    Initialize project

---
+++ src/main/java/com/bot/social/media/botSocialMedia/BotSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.bot.social.media.botSocialMedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSocialMediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotSocialMediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bot/social/media/botSocialMedia/BotSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.social.media.botSocialMedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
