3f86a674ae89b4e33c0700f3d5f3f2490730b6bd
Manuel Bedoya <manuelfernandobedoya@gmail.com>
2024-01-30 11:49:45 -0500

    Change infoi

--- src/main/java/com/bot/social/media/botSocialMedia/config/googlesheets/GoogleAuthorizeUtil.java
+++ src/main/java/com/bot/social/media/botSocialMedia/config/googlesheets/GoogleAuthorizeUtil.java
@@ -1,19 +1,18 @@
 package com.bot.social.media.botSocialMedia.config.googlesheets;
 
 import com.bot.social.media.botSocialMedia.domain.usecase.instagram.InstagramUseCaseImpl;
-import com.bot.social.media.botSocialMedia.infraestructure.entryPoints.instagram.InstagramController;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.sheets.v4.Sheets;
 import com.google.api.services.sheets.v4.SheetsScopes;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -27,6 +26,9 @@ public class GoogleAuthorizeUtil {
     private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
     private static final String CREDENTIALS = "/credentials.json";
     private static final String TOKENS_DIRECTORY = "tokens";
+
+    @Value("${gmail.account}")
+    private static String GMAIL_ACCOUNT;
 
     @Bean
     public static Sheets getSheets() throws IOException,
@@ -49,7 +51,7 @@ public class GoogleAuthorizeUtil {
 
         LocalServerReceiver localServerReceiver = new LocalServerReceiver.Builder().setPort(8888).build();
         Credential credential = new AuthorizationCodeInstalledApp(flow, localServerReceiver)
-                .authorize("manuelfernandobedoya@gmail.com");
+                .authorize(GMAIL_ACCOUNT);
 
         return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), JSON_FACTORY, credential)
                 .setApplicationName("botSocialMedia")
